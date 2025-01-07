fd1a12f63b0b3b32e97b9c4ae73d07fa893f33f0
shinra-electric <50119606+shinra-electric@users.noreply.github.com>
2025-01-07 15:46:26 +0000

    Make quarantine removal recursive in update-macos.sh
    This changes the xattr `-d` flag to `-dr`, which will delete com.apple.quarantine recursively.

--- vita3k/script/update-macos.sh
+++ vita3k/script/update-macos.sh
@@ -10,5 +10,5 @@ hdiutil attach "$DIR"/vita3k-latest.dmg
 cp -Rf -p /Volumes/Vita3K\ Installer/Vita3K.app "$APP_PATH"
 diskutil eject 'Vita3K Installer'
 rm -f "$DIR"/vita3k-latest.dmg
-xattr -d com.apple.quarantine "$APP_PATH"/Vita3K.app
+xattr -dr com.apple.quarantine "$APP_PATH"/Vita3K.app
 open "$APP_PATH"/Vita3K.app
